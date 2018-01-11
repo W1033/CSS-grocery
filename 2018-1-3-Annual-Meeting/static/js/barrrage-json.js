e29ae2de9541f4cd753e17508330cc35a454081e
2416451355@qq.com <314314@wang>
2018-01-11 18:12:58 +0800

    1-11-18:12

--- 2018-1-3-Annual-Meeting/static/js/barrrage-json.js
+++ 2018-1-3-Annual-Meeting/static/js/barrrage-json.js
@@ -2,6 +2,19 @@
 
 
 var brgJson = [
+    {"realName":"王艳凯","sendMsg":"人而无信，不知其可也: 一个人不讲信用，不知道怎么能行"},
+    {"realName":"王艳凯","sendMsg":"0岁闪亮登场"},
+    {"realName":"王艳凯","sendMsg":"10岁茁壮成长"},
+    {"realName":"王艳凯","sendMsg":"20岁为爱痴狂"},
+    {"realName":"王艳凯","sendMsg":"30岁基本定向"},
+    {"realName":"王艳凯","sendMsg":"40岁拼命打闯"},
+    {"realName":"王艳凯","sendMsg":"人而无信，不知其可也"},
+    {"realName":"王艳凯","sendMsg":"50岁回头望望"},
+    {"realName":"王艳凯","sendMsg":"60岁告老还乡"},
+    {"realName":"王艳凯","sendMsg":"70岁搓搓麻将"},
+    {"realName":"王艳凯","sendMsg":"80岁晒晒太阳"},
+    {"realName":"王艳凯","sendMsg":"90岁躺在床上"},
+    {"realName":"王艳凯","sendMsg":"100岁挂在墙上"},
     {"realName":"王艳凯","sendMsg":"0岁闪亮登场"},
     {"realName":"王艳凯","sendMsg":"10岁茁壮成长"},
     {"realName":"王艳凯","sendMsg":"20岁为爱痴狂"},
@@ -13,7 +26,7 @@ var brgJson = [
     {"realName":"王艳凯","sendMsg":"80岁晒晒太阳"},
     {"realName":"王艳凯","sendMsg":"90岁躺在床上"},
     {"realName":"王艳凯","sendMsg":"100岁挂在墙上"},
-    {"realName":"王艳凯","sendMsg":"加油智付1"},
+    /*{"realName":"王艳凯","sendMsg":"加油智付1"},
     {"realName":"王艳凯","sendMsg":"加油智付2"},
     {"realName":"王艳凯","sendMsg":"加油智付3"},
     {"realName":"王艳凯","sendMsg":"加油智付4"},
@@ -48,7 +61,7 @@ var brgJson = [
     {"realName":"王艳凯","sendMsg":"加油智付33"},
     {"realName":"王艳凯","sendMsg":"加油智付34"},
     {"realName":"王艳凯","sendMsg":"加油智付35"},
-    {"realName":"王艳凯","sendMsg":"加油智付36"},
-    {"realName":"王艳凯","sendMsg":"加油智付37"},
-    {"realName":"王艳凯","sendMsg":"加油智付38"}
-]
+    {"realName":"王艳凯","sendMsg":"加油智付36"},*/
+
+
+];
