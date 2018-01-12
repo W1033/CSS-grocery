3199b92fe980875a3f351ce74be7167228c48f7a
2416451355@qq.com <314314@wang>
2018-01-12 18:24:11 +0800

    18-1-12-18:24

--- 2018-1-3-Annual-Meeting/static/js/barrage.js
+++ 2018-1-3-Annual-Meeting/static/js/barrage.js
@@ -2,15 +2,20 @@
 
 function Barrage () {
     // 支持实例化无 new 关键字
-    /* ECMAScript 5 增加了一个新方法，叫 Object.getPrototypeOf()【getPrototypeOf取得原型】,在所有支持的实现中，这个方法返回 [[Prototype]] 的值。例如：
-     * alert(Object.getPrototypeOf(person1) == Person.prototype); //true
-     * --《js高程》第6章: 取得原型对象 */
-    // typeof this === "undefined" 检测 this 是未定义的，
-    // 第二个判断，this 存在但其原型不等于当前构造函数的原型
     if (typeof this === "undefined" || Object.getPrototypeOf(this) !== Barrage.prototype) {
         return new Barrage();
     }
+
+    // 保存对实例的引用
+    var brg;
+    brg = this;
+
+    brg.sequence = {};      // 当前序列
+    brg.uid = 0;            // this.uid
+
+    return brg;
 }
+
 
 /*
 Barrage.prototype.defaults = {
--- 2018-1-3-Annual-Meeting/static/js/barrrage-json.js
+++ 2018-1-3-Annual-Meeting/static/js/barrrage-json.js
@@ -2,66 +2,82 @@
 
 
 var brgJson = [
-    {"realName":"王艳凯","sendMsg":"人而无信，不知其可也: 一个人不讲信用，不知道怎么能行"},
-    {"realName":"王艳凯","sendMsg":"0岁闪亮登场"},
-    {"realName":"王艳凯","sendMsg":"10岁茁壮成长"},
-    {"realName":"王艳凯","sendMsg":"20岁为爱痴狂"},
-    {"realName":"王艳凯","sendMsg":"30岁基本定向"},
-    {"realName":"王艳凯","sendMsg":"40岁拼命打闯"},
-    {"realName":"王艳凯","sendMsg":"人而无信，不知其可也"},
-    {"realName":"王艳凯","sendMsg":"50岁回头望望"},
-    {"realName":"王艳凯","sendMsg":"60岁告老还乡"},
-    {"realName":"王艳凯","sendMsg":"70岁搓搓麻将"},
-    {"realName":"王艳凯","sendMsg":"80岁晒晒太阳"},
-    {"realName":"王艳凯","sendMsg":"90岁躺在床上"},
-    {"realName":"王艳凯","sendMsg":"100岁挂在墙上"},
-    {"realName":"王艳凯","sendMsg":"0岁闪亮登场"},
-    {"realName":"王艳凯","sendMsg":"10岁茁壮成长"},
-    {"realName":"王艳凯","sendMsg":"20岁为爱痴狂"},
-    {"realName":"王艳凯","sendMsg":"30岁基本定向"},
-    {"realName":"王艳凯","sendMsg":"40岁拼命打闯"},
-    {"realName":"王艳凯","sendMsg":"50岁回头望望"},
-    {"realName":"王艳凯","sendMsg":"60岁告老还乡"},
-    {"realName":"王艳凯","sendMsg":"70岁搓搓麻将"},
-    {"realName":"王艳凯","sendMsg":"80岁晒晒太阳"},
-    {"realName":"王艳凯","sendMsg":"90岁躺在床上"},
-    {"realName":"王艳凯","sendMsg":"100岁挂在墙上"},
-    /*{"realName":"王艳凯","sendMsg":"加油智付1"},
-    {"realName":"王艳凯","sendMsg":"加油智付2"},
-    {"realName":"王艳凯","sendMsg":"加油智付3"},
-    {"realName":"王艳凯","sendMsg":"加油智付4"},
-    {"realName":"王艳凯","sendMsg":"加油智付5"},
-    {"realName":"王艳凯","sendMsg":"加油智付6"},
-    {"realName":"王艳凯","sendMsg":"加油智付7"},
-    {"realName":"王艳凯","sendMsg":"加油智付8"},
-    {"realName":"王艳凯","sendMsg":"加油智付9"},
-    {"realName":"王艳凯","sendMsg":"加油智付10"},
-    {"realName":"王艳凯","sendMsg":"加油智付11"},
-    {"realName":"王艳凯","sendMsg":"加油智付12"},
-    {"realName":"王艳凯","sendMsg":"加油智付13"},
-    {"realName":"王艳凯","sendMsg":"加油智付14"},
-    {"realName":"王艳凯","sendMsg":"加油智付15"},
-    {"realName":"王艳凯","sendMsg":"加油智付16"},
-    {"realName":"王艳凯","sendMsg":"加油智付17"},
-    {"realName":"王艳凯","sendMsg":"加油智付18"},
-    {"realName":"王艳凯","sendMsg":"加油智付19"},
-    {"realName":"王艳凯","sendMsg":"加油智付20"},
-    {"realName":"王艳凯","sendMsg":"加油智付21"},
-    {"realName":"王艳凯","sendMsg":"加油智付22"},
-    {"realName":"王艳凯","sendMsg":"加油智付23"},
-    {"realName":"王艳凯","sendMsg":"加油智付24"},
-    {"realName":"王艳凯","sendMsg":"加油智付25"},
-    {"realName":"王艳凯","sendMsg":"加油智付26"},
-    {"realName":"王艳凯","sendMsg":"加油智付27"},
-    {"realName":"王艳凯","sendMsg":"加油智付28"},
-    {"realName":"王艳凯","sendMsg":"加油智付29"},
-    {"realName":"王艳凯","sendMsg":"加油智付30"},
-    {"realName":"王艳凯","sendMsg":"加油智付31"},
-    {"realName":"王艳凯","sendMsg":"加油智付32"},
-    {"realName":"王艳凯","sendMsg":"加油智付33"},
-    {"realName":"王艳凯","sendMsg":"加油智付34"},
-    {"realName":"王艳凯","sendMsg":"加油智付35"},
-    {"realName":"王艳凯","sendMsg":"加油智付36"},*/
-
-
+    {
+        "realName": "王艳凯",
+        "sendMsg": "人而无信，不知其可也",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "王艳凯",
+        "sendMsg": "0岁闪亮登场",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "王艳凯",
+        "sendMsg": "10岁茁壮成长",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "王艳凯",
+        "sendMsg": "20岁为爱痴狂",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "王艳凯",
+        "sendMsg": "30岁基本定向",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "王艳凯",
+        "sendMsg": "40岁拼命打闯",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "王艳凯",
+        "sendMsg": "人而无信，不知其可也",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "王艳凯",
+        "sendMsg": "50岁回头望望",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "王艳凯",
+        "sendMsg": "60岁告老还乡",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "王艳凯",
+        "sendMsg": "70岁搓搓麻将",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "王艳凯",
+        "sendMsg": "80岁晒晒太阳",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "王艳凯",
+        "sendMsg": "90岁躺在床上",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "王艳凯",
+        "sendMsg": "100岁挂在墙上",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
 ];
