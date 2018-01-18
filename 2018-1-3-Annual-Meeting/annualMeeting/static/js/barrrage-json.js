c5ef6fca4f10dedc703980b8d1e3f9d0fd6a14b3
Kell-Wang <2416451355@qq.com>
2018-01-18 15:32:40 +0800

    1-18

---
+++ 2018-1-3-Annual-Meeting/annualMeeting/static/js/barrrage-json.js
@@ -0,0 +1,631 @@
+/** Created on 2018/1/11.*/
+
+
+var brgJson = [
+    {
+        "realName": "XXX",
+        "sendMsg": "人而无信，不知其可也",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "学着主宰自己的生活",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "1岁闪亮登场",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "10岁茁壮成长",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "20岁为爱痴狂",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "友情再深，缘分尽了，就成陌路。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "因为缘分而来的东西",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "刻意去找的东西，往往是找不到的。",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "30岁基本定向",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "40岁拼命打闯",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "人而无信，不知其可也",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "XXX",
+        "sendMsg": "回头望望",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "60岁告老还乡",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "世上的事 只要肯用心去学 没有一件是太晚的。",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "不自怜",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "70岁搓搓麻将",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "YYY",
+        "sendMsg": "新年快乐",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "XXX",
+        "sendMsg": "80岁晒晒太阳",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "90岁躺在床上",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "一个人至少拥有一个梦想，有一个理由去坚强。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "心若没有栖息的地方，到哪里都是在流浪。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "100岁挂在墙上",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "从容不迫的举止 比起咄咄逼人的态度",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "YYY",
+        "sendMsg": "天下万物的来和去，都有他的时间。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "YYY",
+        "sendMsg": "人而无信，不知其可也---OK",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "50岁回头望望---OK",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "YYY",
+        "sendMsg": "即使孑然一身，也不算一个太坏的局面。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "XXX",
+        "sendMsg": "人而无信，不知其可也",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "学着主宰自己的生活",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "1岁闪亮登场",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "10岁茁壮成长",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "20岁为爱痴狂",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "友情再深，缘分尽了，就成陌路。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "因为缘分而来的东西",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "刻意去找的东西，往往是找不到的。",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "30岁基本定向",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "40岁拼命打闯",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "人而无信，不知其可也",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "XXX",
+        "sendMsg": "回头望望",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "60岁告老还乡",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "世上的事 只要肯用心去学 没有一件是太晚的。",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "不自怜",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "70岁搓搓麻将",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "YYY",
+        "sendMsg": "新年快乐",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "XXX",
+        "sendMsg": "80岁晒晒太阳",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "90岁躺在床上",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "一个人至少拥有一个梦想，有一个理由去坚强。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "心若没有栖息的地方，到哪里都是在流浪。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "100岁挂在墙上",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "从容不迫的举止 比起咄咄逼人的态度",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "YYY",
+        "sendMsg": "天下万物的来和去，都有他的时间。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "YYY",
+        "sendMsg": "人而无信，不知其可也---OK",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "50岁回头望望---OK",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "YYY",
+        "sendMsg": "即使孑然一身，也不算一个太坏的局面。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "XXX",
+        "sendMsg": "人而无信，不知其可也",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "学着主宰自己的生活",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "1岁闪亮登场",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "10岁茁壮成长",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "20岁为爱痴狂",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "友情再深，缘分尽了，就成陌路。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "因为缘分而来的东西",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "刻意去找的东西，往往是找不到的。",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "30岁基本定向",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "40岁拼命打闯",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "人而无信，不知其可也",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "XXX",
+        "sendMsg": "回头望望",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "60岁告老还乡",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "世上的事 只要肯用心去学 没有一件是太晚的。",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "不自怜",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "70岁搓搓麻将",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "YYY",
+        "sendMsg": "新年快乐",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "XXX",
+        "sendMsg": "80岁晒晒太阳",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "90岁躺在床上",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "一个人至少拥有一个梦想，有一个理由去坚强。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "心若没有栖息的地方，到哪里都是在流浪。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "100岁挂在墙上",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "从容不迫的举止 比起咄咄逼人的态度",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "YYY",
+        "sendMsg": "天下万物的来和去，都有他的时间。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "YYY",
+        "sendMsg": "人而无信，不知其可也---OK",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "50岁回头望望---OK",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "YYY",
+        "sendMsg": "即使孑然一身，也不算一个太坏的局面。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "XXX",
+        "sendMsg": "人而无信，不知其可也",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "学着主宰自己的生活",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "1岁闪亮登场",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "10岁茁壮成长",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "20岁为爱痴狂",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "友情再深，缘分尽了，就成陌路。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "因为缘分而来的东西",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "刻意去找的东西，往往是找不到的。",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "30岁基本定向",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "40岁拼命打闯",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "人而无信，不知其可也",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+    {
+        "realName": "XXX",
+        "sendMsg": "回头望望",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "60岁告老还乡",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "世上的事 只要肯用心去学 没有一件是太晚的。",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "不自怜",
+        "memberId": "1",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "70岁搓搓麻将",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+
+    {
+        "realName": "XXX",
+        "sendMsg": "80岁晒晒太阳",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "XXX",
+        "sendMsg": "90岁躺在床上",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+    {
+        "realName": "YYY",
+        "sendMsg": "一个人至少拥有一个梦想，有一个理由去坚强。",
+        "memberId": "0",
+        "userProfile": "static/img/user-head/default-avatar.png"
+    },
+
+
+];
